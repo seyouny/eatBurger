466a8d813728615e82d8e8b8e2fc407e5fb2617b
Seyoung Yoon <stacey.exp@gmail.com>
2020-07-26 23:09:24 -0700

    reformatted controllers and orm files to match function names

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,30 +5,31 @@ var router = express.Router();
 
 router.get("/",function(req,res){
     burger.selectAll(function(data){
-        var hbsObject = {
+        var burger_data = {
             burgers:data
         };
-        res.render("index",hbsObject)
+        console.log(data)
+        res.render("index",burger_data)
     })
 })
-router.post("/api/cats", function(req, res) {
-    cat.create([
-      "name", "sleepy"
+router.post("/", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
     ], [
-      req.body.name, req.body.sleepy
+      req.body.name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/api/cats/:id", function(req, res) {
+  router.put("/", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
   
-    cat.update({
-      sleepy: req.body.sleepy
+    burger.insert({
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
@@ -38,3 +39,20 @@ router.post("/api/cats", function(req, res) {
       }
     });
   });
+
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,26 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-      orm.selectAll("burgers", function(res) {
+      orm.all("burgers", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
-    insert: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
+    update: function(cols, vals, cb) {
+      orm.update("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
     create: function(objColVals, condition, cb) {
-      orm.createOne("burgers", objColVals, condition, function(res) {
+      orm.create("burgers", objColVals, condition, function(res) {
         cb(res);
       });
     },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
   };
   
   // Export the database functions for the controller (catsController.js).
